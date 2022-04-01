ab3962caf5ceba1713377b1a914a4ca4edfc998a
Metin KONUK <metinkonuk@hotmail.com>
2022-04-01 17:30:32 +0300

    add Users class

---
+++ Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main
+{
+    public class Users
+    {
+        public Users()
+        {
+            users.Add("admin", "admin");
+            users.Add("user", "user");
+        }
+        public Dictionary<string, string> users = new Dictionary<string, string>();
+    }
+}
