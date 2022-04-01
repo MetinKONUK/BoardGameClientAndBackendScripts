21dff8e9c7430c2b0a8c4f7a29f1de85b43eb68d
Metin KONUK <metinkonuk@hotmail.com>
2022-04-01 20:37:29 +0300

    design settings form, add 2 user controls

---
+++ settings_DifficultyLevelUserControl.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace main
+{
+    partial class settings_DifficultyLevelUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.settings_difficultyLevelPanel = new System.Windows.Forms.Panel();
+            this.settings_SaveDifficultyLevelSelectionButton = new System.Windows.Forms.Button();
+            this.settings_DifficultyLevelNormalRadioButton = new System.Windows.Forms.RadioButton();
+            this.settings_DifficultyLevelHardRadioButton = new System.Windows.Forms.RadioButton();
+            this.settings_DifficultyLevelEasyRadioButton = new System.Windows.Forms.RadioButton();
+            this.settings_difficultyLevelPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // settings_difficultyLevelPanel
+            // 
+            this.settings_difficultyLevelPanel.Controls.Add(this.settings_SaveDifficultyLevelSelectionButton);
+            this.settings_difficultyLevelPanel.Controls.Add(this.settings_DifficultyLevelNormalRadioButton);
+            this.settings_difficultyLevelPanel.Controls.Add(this.settings_DifficultyLevelHardRadioButton);
+            this.settings_difficultyLevelPanel.Controls.Add(this.settings_DifficultyLevelEasyRadioButton);
+            this.settings_difficultyLevelPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.settings_difficultyLevelPanel.Location = new System.Drawing.Point(0, 0);
+            this.settings_difficultyLevelPanel.Name = "settings_difficultyLevelPanel";
+            this.settings_difficultyLevelPanel.Size = new System.Drawing.Size(506, 351);
+            this.settings_difficultyLevelPanel.TabIndex = 2;
+            // 
+            // settings_SaveDifficultyLevelSelectionButton
+            // 
+            this.settings_SaveDifficultyLevelSelectionButton.Location = new System.Drawing.Point(3, 72);
+            this.settings_SaveDifficultyLevelSelectionButton.Name = "settings_SaveDifficultyLevelSelectionButton";
+            this.settings_SaveDifficultyLevelSelectionButton.Size = new System.Drawing.Size(75, 23);
+            this.settings_SaveDifficultyLevelSelectionButton.TabIndex = 3;
+            this.settings_SaveDifficultyLevelSelectionButton.Text = "Save";
+            this.settings_SaveDifficultyLevelSelectionButton.UseVisualStyleBackColor = true;
+            // 
+            // settings_DifficultyLevelNormalRadioButton
+            // 
+            this.settings_DifficultyLevelNormalRadioButton.AutoSize = true;
+            this.settings_DifficultyLevelNormalRadioButton.Location = new System.Drawing.Point(3, 26);
+            this.settings_DifficultyLevelNormalRadioButton.Name = "settings_DifficultyLevelNormalRadioButton";
+            this.settings_DifficultyLevelNormalRadioButton.Size = new System.Drawing.Size(58, 17);
+            this.settings_DifficultyLevelNormalRadioButton.TabIndex = 2;
+            this.settings_DifficultyLevelNormalRadioButton.TabStop = true;
+            this.settings_DifficultyLevelNormalRadioButton.Text = "Normal";
+            this.settings_DifficultyLevelNormalRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // settings_DifficultyLevelHardRadioButton
+            // 
+            this.settings_DifficultyLevelHardRadioButton.AutoSize = true;
+            this.settings_DifficultyLevelHardRadioButton.Location = new System.Drawing.Point(3, 49);
+            this.settings_DifficultyLevelHardRadioButton.Name = "settings_DifficultyLevelHardRadioButton";
+            this.settings_DifficultyLevelHardRadioButton.Size = new System.Drawing.Size(48, 17);
+            this.settings_DifficultyLevelHardRadioButton.TabIndex = 1;
+            this.settings_DifficultyLevelHardRadioButton.TabStop = true;
+            this.settings_DifficultyLevelHardRadioButton.Text = "Hard";
+            this.settings_DifficultyLevelHardRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // settings_DifficultyLevelEasyRadioButton
+            // 
+            this.settings_DifficultyLevelEasyRadioButton.AutoSize = true;
+            this.settings_DifficultyLevelEasyRadioButton.Location = new System.Drawing.Point(3, 3);
+            this.settings_DifficultyLevelEasyRadioButton.Name = "settings_DifficultyLevelEasyRadioButton";
+            this.settings_DifficultyLevelEasyRadioButton.Size = new System.Drawing.Size(48, 17);
+            this.settings_DifficultyLevelEasyRadioButton.TabIndex = 0;
+            this.settings_DifficultyLevelEasyRadioButton.TabStop = true;
+            this.settings_DifficultyLevelEasyRadioButton.Text = "Easy";
+            this.settings_DifficultyLevelEasyRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // settings_DifficultyLevelUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.settings_difficultyLevelPanel);
+            this.Name = "settings_DifficultyLevelUserControl";
+            this.Size = new System.Drawing.Size(506, 351);
+            this.settings_difficultyLevelPanel.ResumeLayout(false);
+            this.settings_difficultyLevelPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel settings_difficultyLevelPanel;
+        private System.Windows.Forms.Button settings_SaveDifficultyLevelSelectionButton;
+        private System.Windows.Forms.RadioButton settings_DifficultyLevelNormalRadioButton;
+        private System.Windows.Forms.RadioButton settings_DifficultyLevelHardRadioButton;
+        private System.Windows.Forms.RadioButton settings_DifficultyLevelEasyRadioButton;
+    }
+}
