a0460655a7a4f883e6934fe78ef80fe83d3134a5
Metin KONUK <metinkonuk@hotmail.com>
2022-04-02 01:53:41 +0300

    add Setting Class

---
+++ Setting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class Setting
+    {
+        public int Row { get; set; }
+        public int Col{ get; set; }
+        public List<int> Shapes{ get; set; }
+        public int DifficultyLevel { get; set; }
+    }
+}
