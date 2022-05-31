3645008613f2c20810b8e3729d847960d5a3ea59
Metin KONUK <metinkonuk@hotmail.com>
2022-06-01 02:59:30 +0300

    add DataFromServer data model to handle retrieving data from node.js server

---
+++ DataFromServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    public class DataFromServer
+    {
+        public string Type { get; set; }
+
+        public List<List<int>> NewShapeCoordinates = new List<List<int>>();
+        public List<int> NewShapeTypes = new List<int>();
+        public bool GameStartedFeedback = false;
+        public bool HomeSpotSetRequestFeedback = false;
+        public bool TargetSpotSetRequestFeedback = false;
+        public List<List<int>> Path = null;
+        public string Turn = null;
+        public string ErrorMessage = null;
+        public List<int> ClearSpotsData = null;
+        public string GameEndInfo = null;
+    }
+}
