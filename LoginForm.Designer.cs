18d4cb4d788cfd7c64014220d333d041f201ae9e
Metin KONUK <metinkonuk@hotmail.com>
2022-06-01 03:46:05 +0300

    design login form

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -44,10 +44,10 @@
             // 
             // login_leftPanel
             // 
+            this.login_leftPanel.Controls.Add(this.Login_TogglePasswordVisibilityCheckBox);
             this.login_leftPanel.Controls.Add(this.LoginForm_MultiplayerModeCheckBox);
             this.login_leftPanel.Controls.Add(this.Login_RegisterLabel);
             this.login_leftPanel.Controls.Add(this.Login_HaveAnAccountLabel);
-            this.login_leftPanel.Controls.Add(this.Login_TogglePasswordVisibilityCheckBox);
             this.login_leftPanel.Controls.Add(this.login_passwordLabel);
             this.login_leftPanel.Controls.Add(this.login_usernameLabel);
             this.login_leftPanel.Controls.Add(this.login_loginButton);
@@ -56,7 +56,7 @@
             this.login_leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.login_leftPanel.Location = new System.Drawing.Point(0, 0);
             this.login_leftPanel.Name = "login_leftPanel";
-            this.login_leftPanel.Size = new System.Drawing.Size(201, 450);
+            this.login_leftPanel.Size = new System.Drawing.Size(231, 374);
             this.login_leftPanel.TabIndex = 0;
             // 
             // LoginForm_MultiplayerModeCheckBox
@@ -66,10 +66,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.LoginForm_MultiplayerModeCheckBox.AutoSize = true;
             this.LoginForm_MultiplayerModeCheckBox.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.LoginForm_MultiplayerModeCheckBox.Location = new System.Drawing.Point(0, 216);
+            this.LoginForm_MultiplayerModeCheckBox.Font = new System.Drawing.Font("Times New Roman", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginForm_MultiplayerModeCheckBox.Location = new System.Drawing.Point(-3, 146);
             this.LoginForm_MultiplayerModeCheckBox.Name = "LoginForm_MultiplayerModeCheckBox";
             this.LoginForm_MultiplayerModeCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.LoginForm_MultiplayerModeCheckBox.Size = new System.Drawing.Size(109, 17);
+            this.LoginForm_MultiplayerModeCheckBox.Size = new System.Drawing.Size(131, 20);
             this.LoginForm_MultiplayerModeCheckBox.TabIndex = 0;
             this.LoginForm_MultiplayerModeCheckBox.TabStop = false;
             this.LoginForm_MultiplayerModeCheckBox.Text = ":Multiplayer Mode";
@@ -79,10 +81,12 @@
             // 
             this.Login_RegisterLabel.AutoSize = true;
             this.Login_RegisterLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Login_RegisterLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Login_RegisterLabel.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.Login_RegisterLabel.Location = new System.Drawing.Point(145, 298);
+            this.Login_RegisterLabel.Location = new System.Drawing.Point(178, 261);
             this.Login_RegisterLabel.Name = "Login_RegisterLabel";
-            this.Login_RegisterLabel.Size = new System.Drawing.Size(46, 13);
+            this.Login_RegisterLabel.Size = new System.Drawing.Size(53, 16);
             this.Login_RegisterLabel.TabIndex = 0;
             this.Login_RegisterLabel.Text = "Register";
             this.Login_RegisterLabel.Click += new System.EventHandler(this.Login_RegisterLabel_Click);
@@ -90,9 +94,10 @@
             // Login_HaveAnAccountLabel
             // 
             this.Login_HaveAnAccountLabel.AutoSize = true;
-            this.Login_HaveAnAccountLabel.Location = new System.Drawing.Point(-3, 298);
+            this.Login_HaveAnAccountLabel.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Login_HaveAnAccountLabel.Location = new System.Drawing.Point(3, 258);
             this.Login_HaveAnAccountLabel.Name = "Login_HaveAnAccountLabel";
-            this.Login_HaveAnAccountLabel.Size = new System.Drawing.Size(126, 13);
+            this.Login_HaveAnAccountLabel.Size = new System.Drawing.Size(175, 19);
             this.Login_HaveAnAccountLabel.TabIndex = 0;
             this.Login_HaveAnAccountLabel.Text = "Don\'t Have An Account?";
             // 
@@ -100,9 +105,10 @@
             // 
             this.Login_TogglePasswordVisibilityCheckBox.AutoSize = true;
             this.Login_TogglePasswordVisibilityCheckBox.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Login_TogglePasswordVisibilityCheckBox.Location = new System.Drawing.Point(148, 194);
+            this.Login_TogglePasswordVisibilityCheckBox.Font = new System.Drawing.Font("Times New Roman", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Login_TogglePasswordVisibilityCheckBox.Location = new System.Drawing.Point(177, 107);
             this.Login_TogglePasswordVisibilityCheckBox.Name = "Login_TogglePasswordVisibilityCheckBox";
-            this.Login_TogglePasswordVisibilityCheckBox.Size = new System.Drawing.Size(51, 17);
+            this.Login_TogglePasswordVisibilityCheckBox.Size = new System.Drawing.Size(54, 20);
             this.Login_TogglePasswordVisibilityCheckBox.TabIndex = 0;
             this.Login_TogglePasswordVisibilityCheckBox.TabStop = false;
             this.Login_TogglePasswordVisibilityCheckBox.Text = "show";
@@ -113,27 +119,30 @@
             // login_passwordLabel
             // 
             this.login_passwordLabel.AutoSize = true;
-            this.login_passwordLabel.Location = new System.Drawing.Point(12, 176);
+            this.login_passwordLabel.Font = new System.Drawing.Font("Times New Roman", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.login_passwordLabel.Location = new System.Drawing.Point(3, 89);
             this.login_passwordLabel.Name = "login_passwordLabel";
-            this.login_passwordLabel.Size = new System.Drawing.Size(52, 13);
+            this.login_passwordLabel.Size = new System.Drawing.Size(72, 15);
             this.login_passwordLabel.TabIndex = 0;
-            this.login_passwordLabel.Text = "password";
+            this.login_passwordLabel.Text = "PASSWORD";
             // 
             // login_usernameLabel
             // 
             this.login_usernameLabel.AutoSize = true;
-            this.login_usernameLabel.Location = new System.Drawing.Point(12, 137);
+            this.login_usernameLabel.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_usernameLabel.Location = new System.Drawing.Point(2, 35);
             this.login_usernameLabel.Name = "login_usernameLabel";
-            this.login_usernameLabel.Size = new System.Drawing.Size(53, 13);
+            this.login_usernameLabel.Size = new System.Drawing.Size(73, 15);
             this.login_usernameLabel.TabIndex = 0;
-            this.login_usernameLabel.Text = "username";
+            this.login_usernameLabel.Text = "USERNAME";
             // 
             // login_loginButton
             // 
             this.login_loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.login_loginButton.Location = new System.Drawing.Point(0, 239);
+            this.login_loginButton.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_loginButton.Location = new System.Drawing.Point(0, 188);
             this.login_loginButton.Name = "login_loginButton";
-            this.login_loginButton.Size = new System.Drawing.Size(201, 35);
+            this.login_loginButton.Size = new System.Drawing.Size(231, 35);
             this.login_loginButton.TabIndex = 5;
             this.login_loginButton.Text = "Log In";
             this.login_loginButton.UseVisualStyleBackColor = true;
@@ -141,27 +150,27 @@
             // 
             // login_passwordTextBox
             // 
-            this.login_passwordTextBox.Location = new System.Drawing.Point(0, 192);
+            this.login_passwordTextBox.Location = new System.Drawing.Point(-3, 107);
             this.login_passwordTextBox.Name = "login_passwordTextBox";
             this.login_passwordTextBox.PasswordChar = '*';
-            this.login_passwordTextBox.Size = new System.Drawing.Size(201, 20);
+            this.login_passwordTextBox.Size = new System.Drawing.Size(231, 20);
             this.login_passwordTextBox.TabIndex = 2;
             // 
             // login_usernameTextBox
             // 
-            this.login_usernameTextBox.Location = new System.Drawing.Point(0, 153);
+            this.login_usernameTextBox.Location = new System.Drawing.Point(-3, 53);
             this.login_usernameTextBox.Name = "login_usernameTextBox";
             this.login_usernameTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.login_usernameTextBox.Size = new System.Drawing.Size(201, 20);
+            this.login_usernameTextBox.Size = new System.Drawing.Size(231, 20);
             this.login_usernameTextBox.TabIndex = 1;
             this.login_usernameTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Login_usernameTextBox_KeyPress);
             // 
             // Login_ImagePanel
             // 
             this.Login_ImagePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Login_ImagePanel.Location = new System.Drawing.Point(201, 0);
+            this.Login_ImagePanel.Location = new System.Drawing.Point(231, 0);
             this.Login_ImagePanel.Name = "Login_ImagePanel";
-            this.Login_ImagePanel.Size = new System.Drawing.Size(599, 450);
+            this.Login_ImagePanel.Size = new System.Drawing.Size(514, 374);
             this.Login_ImagePanel.TabIndex = 1;
             // 
             // LoginForm
@@ -169,7 +178,7 @@
             this.AcceptButton = this.login_loginButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(745, 374);
             this.Controls.Add(this.Login_ImagePanel);
             this.Controls.Add(this.login_leftPanel);
             this.Name = "LoginForm";
