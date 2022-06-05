9951f150a92a58ac0a5266ab453079ce86759dd2
Hasan YASEN <ysnhasan1@gmail.com>
2022-06-05 15:39:50 +0300

    reformat the about form

--- HelpForm.Designer.cs
+++ HelpForm.Designer.cs
@@ -37,9 +37,10 @@ namespace main
             // HelpForm_AboutButton
             // 
             this.HelpForm_AboutButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HelpForm_AboutButton.Location = new System.Drawing.Point(0, 212);
+            this.HelpForm_AboutButton.Location = new System.Drawing.Point(13, 261);
+            this.HelpForm_AboutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HelpForm_AboutButton.Name = "HelpForm_AboutButton";
-            this.HelpForm_AboutButton.Size = new System.Drawing.Size(75, 23);
+            this.HelpForm_AboutButton.Size = new System.Drawing.Size(100, 28);
             this.HelpForm_AboutButton.TabIndex = 0;
             this.HelpForm_AboutButton.Text = "About";
             this.HelpForm_AboutButton.UseVisualStyleBackColor = true;
@@ -50,23 +51,25 @@ namespace main
             this.HelpForm_InfoTextBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.HelpForm_InfoTextBox.Font = new System.Drawing.Font("Times New Roman", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HelpForm_InfoTextBox.Location = new System.Drawing.Point(0, 0);
+            this.HelpForm_InfoTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HelpForm_InfoTextBox.Multiline = true;
             this.HelpForm_InfoTextBox.Name = "HelpForm_InfoTextBox";
             this.HelpForm_InfoTextBox.ReadOnly = true;
-            this.HelpForm_InfoTextBox.Size = new System.Drawing.Size(800, 206);
+            this.HelpForm_InfoTextBox.Size = new System.Drawing.Size(1067, 253);
             this.HelpForm_InfoTextBox.TabIndex = 0;
             this.HelpForm_InfoTextBox.TabStop = false;
             this.HelpForm_InfoTextBox.Text = resources.GetString("HelpForm_InfoTextBox.Text");
             // 
             // HelpForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.HelpForm_InfoTextBox);
             this.Controls.Add(this.HelpForm_AboutButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "HelpForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HelpForm";
